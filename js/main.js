aa1189f09a6b9f22068a62a42d81a8d9951516b1
Arturchukk <111747631+Arturchukk@users.noreply.github.com>
2022-12-07 03:26:53 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+"use stript";
+console.log('Я учу JS');
+
+let firstName = 'Артур';
+console.log(firstName);
+let lastName = 'Frait';
+console.log(lastName);
+let age = 18;
+console.log(age);
+age = 19;
+console.log(age);
